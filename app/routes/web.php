fefffa92725ce1f770415bcc330b18299aac8f12
Rinshiryu <kennynarukenzy@gmail.com>
2026-06-25 07:38:03 +0700

    backend #1
    Backend logic untuk login dan register

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,13 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('login');
 });
+Route::post('/login', [AuthController::class, 'login'])
+    ->name('login');
 Route::get('/main', function () {
+    if (!session()->has('id_akun')) {
+        return redirect('/');
+    }
     return view('main');
 });
-Route::get('/', function () {
+Route::get('/register', function () {
     return view('regis');
 });
+Route::post('/register', [AuthController::class, 'register'])
+    ->name('register');
